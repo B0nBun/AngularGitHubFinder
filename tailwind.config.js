f85e3f7b07528a8d67adf4172201cb14093e5881
B0nBun <rgusiakov@gmail.com>
2022-12-09 01:35:57 +0300

    refactor(app,styles.css): replaced css files with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gh-gray-50":  "#f6f8fa",
+        "gh-gray-100": "#eaeef2",
+        "gh-gray-200": "#d0d7de",
+        "gh-gray-300": "#afb8c1",
+        "gh-gray-400": "#8c959f",
+        "gh-gray-500": "#6e7781",
+        "gh-gray-600": "#57606a",
+        "gh-gray-700": "#424a53",
+        "gh-gray-800": "#32383f",
+        "gh-gray-900": "#24292f",
+      }
+    },
   },
   plugins: [],
 }
